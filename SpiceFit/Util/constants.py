4d9963ae8e932f63f08a2c36b2ad0303c01197e5
dolliou <dolliou@mps.mpg.de>
2025-02-12 17:02:44 +0100

    removed util folder

--- SpiceFit/Util/constants.py
+++
@@ -1,6 +0,0 @@
-class Constants:
-    conventional_lambda_units = "nm"
-    conventional_spectral_units = "W/ (m2 sr nm)"
-    conventional_velocity_units = "km/s"
-    conventional_radiance_units = "W/ (m2 sr)"
-    inch_to_cm = 1 / 2.54

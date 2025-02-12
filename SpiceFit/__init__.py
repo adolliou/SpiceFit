a36701501bf0ebab27d6307bd2368541040f2828
dolliou <dolliou@mps.mpg.de>
2025-02-12 17:43:20 +0100

    try debugging path issues

--- SpiceFit/__init__.py
+++ SpiceFit/__init__.py
@@ -1,6 +1,2 @@
 from .util import *
 from .core import *
-
-import os
-
- lines_templates_path = os.path.join(os.path.dirname(__file__), 'TemplateLines')
--- SpiceFit/core/FittingModel.py
+++ SpiceFit/core/FittingModel.py
@@ -12,7 +12,7 @@ import os
 import importlib.util
 import sys
 import secrets
-
+from pathlib import Path
 
 class FittingModel:
     bound_equation_re = re.compile(
@@ -65,7 +65,7 @@ class FittingModel:
                 if os.path.isfile(filename):
                     self.filename = filename
                 else:
-                    basis_yaml_templates_file = os.path.join(lines_templates_path, filename)
+                    basis_yaml_templates_file = os.path.join(Path(__file__).parents[1], "TemplatesLines", filename)
                     if not os.path.isfile(basis_yaml_templates_file):
                         raise ValueError("Could not find the template yaml for the given line")
                     self.filename = basis_yaml_templates_file

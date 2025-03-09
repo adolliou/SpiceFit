8ec0ae3d4116a3c6cda1a48ad617e494169c3ae4
adolliou <antoine.dolliou@universite-paris-saclay.fr>
2025-03-09 21:31:25 +0100

    added main_line attribute to FitResults.py

--- SpiceFit/core/FitResults.py
+++ SpiceFit/core/FitResults.py
@@ -1007,8 +1007,7 @@ class FitResults:
                 title = f"{str(xpos[ii]), str(ypos[ii])}, "
                 for param in self.components_results["main"]["coeffs"].keys():
                     if (param != "radiance") and (param != "velocity") and (param != "s"):
-                        title += f"{param} : {self.components_results['main']['coeffs'][param]['results'].value[0,
-                        ypos[ii], xpos[ii]]:.2f}, "
+                        title += f"{param} : {self.components_results['main']['coeffs'][param]['results'].value[0,ypos[ii], xpos[ii]]:.2f}, "
                 axs[1].set_title(title)
 
                 pdf.savefig(fig)

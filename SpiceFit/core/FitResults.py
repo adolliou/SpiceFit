Added the correct formula for chi2

SpiceFit/core/FitResults.py
@@ -546,13 +546,12 @@ class FitResults:
             dy = uncertainty_cube[t, :, i, j]
 
             try:
-                popt, pcov = fit_spectra(x=x,
+                popt, pcov, chi2 = fit_spectra(x=x,
                                          y=y,
                                          dy=dy,
                                          fit_template=fit_template,
                                          minimum_data_points=minimum_data_points)
 
-                chi2 = np.sum(np.diag(pcov))
                 if chi2 <= chi_limit:
                     self.fit_results["coeff"][:, t, i, j] = popt
                     self.fit_results["coeffs_error"][:, t, i, j] = np.sqrt(np.diag(pcov))

SpiceFit/core/fitting_spectra.py
@@ -26,7 +26,10 @@ def fit_spectra(x, y, dy, fit_template: FittingModel, minimum_data_points: int =
                 full_output=False,
                 absolute_sigma=True,
                 # jac=fit_template.jacobian_function,
-            )
+            )   
+            r = y - fit_template.fitting_function(x, *popt)
+            chi2 = (1/len(x)) * np.sum((r/dy)**2)
+
         # from matplotlib import pyplot as plt
         # y2 = fit_template.fitting_function(x, *fit_template.params_free["guess"])
         # y3 = fit_template.fitting_function(x, *popt)
@@ -38,6 +41,6 @@ def fit_spectra(x, y, dy, fit_template: FittingModel, minimum_data_points: int =
             if verbose == 2:
                 print("Fitting failed")
 
-            return None, None
+            return None, None, None
 
-        return popt, pcov
+        return popt, pcov, chi2